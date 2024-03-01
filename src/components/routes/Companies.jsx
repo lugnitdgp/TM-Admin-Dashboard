a9d16bdfbd5fff6a2403d35efa66b44d911cab6a
Pratik Chandra Pal <pratikpal050@gmail.com>
2024-03-01 18:05:50 +0530

    companies table changed

--- src/components/routes/Companies.jsx
+++ src/components/routes/Companies.jsx
@@ -1,15 +1,13 @@
 import products from "../../../products.json";
 function Companies() {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center justify-end  w-full">
-        <div></div>
-        <button className="text-sm text-white text-wrap right-4 w-30 h-15 rounded-md bg-blue-500 p-2">
-          Create New Company
+    <div>
+      <div className="flex flex-col items-center w-full ml-48">
+        <button className="text-md ml-auto mb-4 text-white text-wrap right-4 w-30 h-15 rounded-md bg-blue-500 p-2">
+          Add New Product
         </button>
-      </div>
-      <div className="ml-96 overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+      <div className="ml-96 overflow-x-auto w-full">
+        <table className="w-full text-lg text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
@@ -29,15 +27,15 @@ function Companies() {
           <tbody>
             {products.map((product, index) => (
               <tr
-                key={index}
-                className={`bg-white border-b dark:bg-gray-800 ${
-                  index % 2 === 0 ? "bg-gray-100 dark:bg-gray-700" : ""
-                }`}
+              key={index}
+              className={`bg-white border-b dark:bg-gray-800 ${
+                index % 2 === 0 ? "bg-gray-100 dark:bg-gray-700" : ""
+              }`}
               >
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
+                  >
                   {product.name}
                 </th>
                 <td className="px-6 py-4">{product.color}</td>
@@ -48,6 +46,7 @@ function Companies() {
           </tbody>
         </table>
       </div>
+    </div>
     </div>
   );
 }
